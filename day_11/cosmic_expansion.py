042abb115e70b21bb45fdac45eec960a666b0874
daniel <daniel@tfc.ai>
2023-12-11 20:05:52 +0800

    cleanup

--- day_11/cosmic_expansion.py
+++ day_11/cosmic_expansion.py
@@ -13,47 +13,29 @@ def create_map(input) -> np.array:
         game_map[idx] = np.array([char == "#" for char in line], dtype='bool')
     return game_map
 
-def expand_space(game_map: np.array) -> np.array:
-    global empty_cols, empty_rows
-    empty_rows = np.argwhere(np.invert(np.apply_along_axis(np.any, axis=1, arr=game_map))).flatten()
-    empty_cols = np.argwhere(np.invert(np.apply_along_axis(np.any, axis=0, arr=game_map))).flatten()
-    game_map = np.insert(game_map, empty_rows, False, axis=0)
-    game_map = np.insert(game_map, empty_cols, False, axis=1)
-    return game_map
+game_map = create_map(input)
+empty_rows = np.argwhere(np.invert(np.apply_along_axis(np.any, axis=1, arr=game_map))).flatten()
+empty_cols = np.argwhere(np.invert(np.apply_along_axis(np.any, axis=0, arr=game_map))).flatten()
+
 
 def get_dist(edge: tuple) -> int:
     return np.abs(edge[0][0] - edge[1][0]) + np.abs(edge[0][1] - edge[1][1])
 
-game_map = create_map(input)
-expanded_map = expand_space(game_map)
 
+# updated + part 1 + 2
+multipliers = (1, 1_000_000 - 1)
+for j, expansion_multiplier in enumerate(multipliers):
+    G = nx.Graph()
+    for node in np.argwhere(game_map):
+        row_multiplier = np.searchsorted(empty_rows, node[0]) 
+        col_multiplier = np.searchsorted(empty_cols, node[1])
 
+        row_multiplier = row_multiplier * expansion_multiplier if row_multiplier >= 1 else 0 # multiply with 0 -> 0
+        col_multiplier = col_multiplier * expansion_multiplier if col_multiplier >= 1 else 0
 
-G = nx.Graph()
-for node in np.argwhere(expanded_map):
-    G.add_node((node[0], node[1]))
+        G.add_node((node[0] + row_multiplier, node[1] + col_multiplier))
 
-CG: nx.Graph = nx.complete_graph(G.nodes)
-i = sum([get_dist(edge) for edge in CG.edges])
-print(i)
-
-# part 2
-expansion_multiplier: int = 1_000_000 - 1
-G = nx.Graph()
-for node in np.argwhere(game_map):
-    row_multiplier = np.searchsorted(empty_rows, node[0]) 
-    col_multiplier = np.searchsorted(empty_cols, node[1])
-    print(f"row:", row_multiplier, f" {node=} {empty_rows=}")
-    print(f"col:", col_multiplier, f" {node=}, {empty_cols=}")
-
-    row_multiplier = row_multiplier * expansion_multiplier if row_multiplier >= 1 else 0 # multiply with 0 -> 0
-    col_multiplier = col_multiplier * expansion_multiplier if col_multiplier >= 1 else 0
-
-    G.add_node((node[0] + row_multiplier, node[1] + col_multiplier))
-
-print(G.nodes)
-
-CG: nx.Graph = nx.complete_graph(G.nodes)
-i = sum([get_dist(edge) for edge in CG.edges])
-print(i)
+    CG: nx.Graph = nx.complete_graph(G.nodes)
+    i = sum([get_dist(edge) for edge in CG.edges])
+    print(f"part {j+1}: {i}")
 
