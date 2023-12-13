4ce041ee2e1f819a404f843875adebd109015fa2
daniel <daniel@tfc.ai>
2023-12-13 14:55:42 +0800

    day 13 / 1 (python)

--- day_11/cosmic_expansion.py
+++ day_11/cosmic_expansion.py
@@ -1,6 +1,6 @@
 import numpy as np
 import itertools
-import operator
+import multiprocessing
 
 def main():
     input = [line for line in open("input.txt").readlines() if len(line) > 0]
@@ -24,18 +24,18 @@ def main():
     for j, expansion_multiplier in enumerate(multipliers):
         G = []
         for node in np.argwhere(game_map):
-            row_multiplier = np.searchsorted(empty_rows, node[0]) 
-            col_multiplier = np.searchsorted(empty_cols, node[1])
-
-            row_multiplier = row_multiplier * expansion_multiplier
-            col_multiplier = col_multiplier * expansion_multiplier
-
-            G.append((node[0] + row_multiplier, node[1] + col_multiplier))
+            G.append((node[0] + np.searchsorted(empty_rows, node[0]) * expansion_multiplier,
+                      node[1] + np.searchsorted(empty_cols, node[1]) * expansion_multiplier))
 
         all_distances = 0
-        for i in itertools.combinations(G, 2):
-             all_distances += abs(i[0][0] - i[1][0]) + abs(i[0][1] - i[1][1])
+        pool = multiprocessing.Pool()
+        pool.map(lambda x: x[0][0] + x[0][1], tuple(itertools.combinations(G, 2)))
+        # for i in itertools.combinations(G, 2):
+        #      all_distances += abs(i[0][0] - i[1][0]) + abs(i[0][1] - i[1][1])
         print(f"part {j+1}: {all_distances}")
+
+def get_dist(i):
+    return abs(i[0][0] - i[1][0]) + abs(i[0][1] - i[1][1])
 
 
 if __name__ == "__main__":
